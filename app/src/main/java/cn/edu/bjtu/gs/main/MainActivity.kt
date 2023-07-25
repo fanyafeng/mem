c1b7a45c29d59a51e7bd3f2bfede0a793594bb84
fanyafeng <fanyafeng@live.cn>
2023-07-25 16:21:34 +0800

    Author: fanyafeng Email: fanyafeng@live.cn Modify
    1.five pages have been finished

--- app/src/main/java/cn/edu/bjtu/gs/main/MainActivity.kt
+++ app/src/main/java/cn/edu/bjtu/gs/main/MainActivity.kt
@@ -3,10 +3,17 @@ package cn.edu.bjtu.gs.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import cn.edu.bjtu.gs.BaseActivity
 import cn.edu.bjtu.gs.R
 import cn.edu.bjtu.gs.databinding.ActivityMainBinding
+import cn.edu.bjtu.gs.main.addresspage.AddressPageFragment
+import cn.edu.bjtu.gs.main.homepage.HomePageFragment
 import cn.edu.bjtu.gs.main.login.api.LoginPostParam
+import cn.edu.bjtu.gs.main.messagepage.MessagePageFragment
+import cn.edu.bjtu.gs.main.minepage.MinePageFragment
+import cn.edu.bjtu.gs.main.publishpage.PublishPageFragment
 import cn.edu.bjtu.gs.main.register.api.RegisterPostParam
 import cn.edu.bjtu.gs.main.register.api.RegisterResponse
 import com.ripple.http.extend.httpPost
@@ -15,33 +22,120 @@ class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var mainTabModelList = mutableListOf<MainTabModel>()
+    private var mainFragmentList = mutableListOf<Fragment>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
         initView()
         initData()
     }
 
     private fun initView() {
-        binding.message.text = "首页融合"
-        binding.message.setOnClickListener {
-//            httpPost {
-//                params = RegisterPostParam()
-//
-//                onSuccess<RegisterResponse> {
-//                    Toast.makeText(this@MainActivity, "注册成功", Toast.LENGTH_SHORT).show()
-//                }
-//
-//                onFailed {
-//                    Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
-//                }
-//            }
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        mainFragmentList.apply {
+            add(HomePageFragment().apply {
+                fragmentTransaction.add(R.id.container, this, "1")
+            })
+            add(AddressPageFragment().apply {
+                fragmentTransaction.add(R.id.container, this, "2")
+            })
+            add(PublishPageFragment().apply {
+                fragmentTransaction.add(R.id.container, this, "3")
+            })
+            add(MessagePageFragment().apply {
+                fragmentTransaction.add(R.id.container, this, "4")
+            })
+            add(MinePageFragment().apply {
+                fragmentTransaction.add(R.id.container, this, "5")
+            })
         }
+        fragmentTransaction.commit()
+
+        mainTabModelList.apply {
+            clear()
+            add(
+                MainTabModel(
+                    0,
+                    binding.homePageIcon,
+                    binding.homePageTitle,
+                    binding.homePageGroup
+                ).apply {
+                    mainTabModelList = this@MainActivity.mainTabModelList
+                    mainFragmentList = this@MainActivity.mainFragmentList
+                    fragmentManager = this@MainActivity.supportFragmentManager
+                }
+            )
+            add(
+                MainTabModel(
+                    1,
+                    binding.addressPageIcon,
+                    binding.addressPageTitle,
+                    binding.addressPageGroup
+                ).apply {
+                    mainTabModelList = this@MainActivity.mainTabModelList
+                    mainFragmentList = this@MainActivity.mainFragmentList
+                    fragmentManager = this@MainActivity.supportFragmentManager
+                }
+            )
+            add(
+                MainTabModel(
+                    2,
+                    binding.publishPageIcon,
+                    binding.publishPageTitle,
+                    binding.publishPageGroup
+                ).apply {
+                    mainTabModelList = this@MainActivity.mainTabModelList
+                    mainFragmentList = this@MainActivity.mainFragmentList
+                    fragmentManager = this@MainActivity.supportFragmentManager
+                }
+            )
+            add(
+                MainTabModel(
+                    3,
+                    binding.messagePageIcon,
+                    binding.messagePageTitle,
+                    binding.messagePageGroup
+                ).apply {
+                    mainTabModelList = this@MainActivity.mainTabModelList
+                    mainFragmentList = this@MainActivity.mainFragmentList
+                    fragmentManager = this@MainActivity.supportFragmentManager
+                }
+            )
+            add(
+                MainTabModel(
+                    4,
+                    binding.minePageIcon,
+                    binding.minePageTitle,
+                    binding.minePageGroup
+                ).apply {
+                    mainTabModelList = this@MainActivity.mainTabModelList
+                    mainFragmentList = this@MainActivity.mainFragmentList
+                    fragmentManager = this@MainActivity.supportFragmentManager
+                }
+            )
+        }
+        mainTabModelList[0].setChecked()
+
+        binding.homePageGroup.performClick()
     }
 
     private fun initData() {
 
+    }
+
+    private fun selectTab(selectPosition: Int) {
+        mainTabModelList.forEachIndexed { index, mainTabModel ->
+            if (index == selectPosition) {
+                mainTabModel.setChecked()
+            } else {
+                mainTabModel.setUnChecked()
+            }
+        }
     }
 }
---
+++ app/src/main/java/cn/edu/bjtu/gs/main/MainTabModel.kt
@@ -0,0 +1,93 @@
+package cn.edu.bjtu.gs.main
+
+import android.graphics.Color
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.Group
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.ripple.log.tpyeextend.toLogD
+
+
+/**
+ * Author: fanyafeng
+ * Data: 2023/7/25 09:31
+ * Email: fanyafeng@live.cn
+ * Description:
+ *                                   _ooOoo_
+ *                                  o8888888o
+ *                                  88" . "88
+ *                                  (| -_- |)
+ *                                   O\ = /O
+ *                               ____/`---'\____
+ *                             .   ' \\| |// `.
+ *                              / \\||| : |||// \
+ *                            / _||||| -:- |||||- \
+ *                              | | \\\ - /// | |
+ *                            | \_| ''\---/'' | |
+ *                             \ .-\__ `-` ___/-. /
+ *                          ___`. .' /--.--\ `. . __
+ *                       ."" '< `.___\_<|>_/___.' >'"".
+ *                      | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *                        \ \ `-. \_ __\ /__ _/ .-` / /
+ *                ======`-.____`-.___\_____/___.-`____.-'======
+ *                                   `=---='
+ *                .............................................
+ *                         佛祖保佑             永无BUG
+ *                 佛曰:
+ *                         写字楼里写字间，写字间里程序员；
+ *                         程序人员写程序，又拿程序换酒钱。
+ *                         酒醒只在网上坐，酒醉还来网下眠；
+ *                         酒醉酒醒日复日，网上网下年复年。
+ *                         但愿老死电脑间，不愿鞠躬老板前；
+ *                         奔驰宝马贵者趣，公交自行程序员。
+ *                         别人笑我忒疯癫，我笑自己命太贱；
+ *                         不见满街漂亮妹，哪个归得程序员？
+ *///Github See: https://github.com/fanyafeng
+
+
+data class MainTabModel(
+    var position: Int,
+    var icon: ImageView,
+    var title: TextView,
+    val group: View
+) {
+
+    var mainTabModelList: MutableList<MainTabModel>? = null
+    var mainFragmentList: MutableList<Fragment>? = null
+    var fragmentManager: FragmentManager? = null
+
+    init {
+        group.setOnClickListener { _ ->
+            mainTabModelList?.forEachIndexed { index, mainTabModel ->
+                if (index == position) {
+                    mainTabModel.setChecked()
+                    val fragmentTransaction = fragmentManager?.beginTransaction()
+                    mainFragmentList?.get(position)?.let { fragment ->
+                        fragmentTransaction?.show(fragment)
+                        fragmentTransaction?.commit()
+                    }
+
+                } else {
+                    mainTabModel.setUnChecked()
+                    val fragmentTransaction = fragmentManager?.beginTransaction()
+                    mainFragmentList?.get(index)?.let { fragment ->
+                        fragmentTransaction?.hide(fragment)
+                        fragmentTransaction?.commit()
+                    }
+                }
+            }
+        }
+    }
+
+
+    fun setChecked() {
+        title.setTextColor(Color.BLUE)
+    }
+
+    fun setUnChecked() {
+        title.setTextColor(Color.BLACK)
+    }
+}
